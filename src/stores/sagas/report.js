b0ed9cafdf1642c8377009e0326dc3b27df0dd1c
A Shuai <lishuai28@meituan.com>
2020-03-08 07:37:31 +0800

    feat: 优化ui，补全查询saga逻辑

--- src/stores/sagas/report.js
+++ src/stores/sagas/report.js
@@ -1,31 +1,35 @@
 import { all, put, select, takeLatest } from 'redux-saga/effects'
 import { message } from 'antd'
 import { REPORT_ACTIONS } from '../actions'
+import { isEmptyStr } from '../../utils'
 
 function* handleQuery() {
     try {
         const state = yield select()
         const report = state.report
-        if (report.get('querying')) {
+        if (report.get('loading')) {
             // 已经在请求中，则跳过
             return
         }
 
-        yield put(REPORT_ACTIONS.toggleQuerying(true))
+        yield put(REPORT_ACTIONS.toggleLoading(true))
 
         // 校验参数
-
+        const area = report.get('area')
+        if (isEmptyStr(area)) {
+            throw new Error('地区选择不得为空')
+        }
 
         // TODO 网络请求
 
 
         message.info('查询成功')
-        yield put(REPORT_ACTIONS.toggleQuerying(false))
+        yield put(REPORT_ACTIONS.toggleLoading(false))
     } catch (e) {
         if (e.message) {
             message.info(e.message)
         }
-        yield put(REPORT_ACTIONS.toggleQuerying(false))
+        yield put(REPORT_ACTIONS.toggleLoading(false))
     }
 }
 
